f0cd9775206a3aaef16998aabdb93813af13026b
Thiem <thiem.huynh0@gmail.com>
2023-03-21 15:22:25 -0400

    basic server, removed express code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const http = require('http');
 
 const server = http.createServer(async (req, res) => {
     if (req.method === 'GET' && req.url === '/'){
-        res.statusCode(200)
+        console.log('hello from server')
         res.end()
     }
+})
+
+server.listen(3001, () => {
+    console.log('server on http://localhost:3001')
 })
