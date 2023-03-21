201f21009fb99384601af2c29eff7a2943f3a5fa
Thiem <thiem.huynh0@gmail.com>
2023-03-21 15:40:43 -0400

    server require express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-const http = require('http');
+const app = require('./server');
 
-const server = http.createServer(async (req, res) => {
-    if (req.method === 'GET' && req.url === '/'){
-        console.log('hello from server')
-        res.end()
-    }
-})
-
-server.listen(3001, () => {
-    console.log('server on http://localhost:3001')
+app.listen(3001, () => {
+    console.log('hello on http://localhost:3001')
 })
