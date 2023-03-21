09b4d58333d20a5a1ad5c0b60852da9099faf09e
Thiem <thiem.huynh0@gmail.com>
2023-03-21 15:13:58 -0400

    barebones backend

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET' && req.url === '/'){
+        res.statusCode(200)
+        res.end()
+    }
+})
