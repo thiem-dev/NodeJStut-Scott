dd58952e97b519fae6745c608160515fd266c7a6
Thiem <thiem.huynh0@gmail.com>
2023-03-27 15:31:23 -0400

    verifying jwt token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -10,12 +10,32 @@ export const createJWT = (user) => {
     return token
 }
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
     const bearer = req.headers.authorization
 
     if(!bearer){
         res.status(401)
         res.json({message: 'User not authorized'})
+        return
+    }
+
+    const [, token] = bearer.split(' ') 
+
+    if(!token){
+        res.status(401)
+        res.json({message: 'invalid token'})
+        return
+    }
+
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = user
+        next()
+    } 
+    catch (e) {
+        console.error(e)
+        res.status(401)
+        res.json({message: 'invalid token'})
         return
     }
 }
