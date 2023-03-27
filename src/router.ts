dab17ae92b00a919a1f9acf89d383bbaf56c2006
Thiem <thiem.huynh0@gmail.com>
2023-03-27 15:10:59 -0400

    bcrypt and dotenv

--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,7 @@ const router = Router()
 */ 
 
 router.get('/product', (req, res) => {
-    res.json({message: req.thiem_secret})
+    res.json({message: 'message'})
 })
 router.get('/product/:id', () => {})
 router.put('/product/:id', () => {})
