d17dcf7edbc97c01ea115a560b4766a63fca6eb2
Thiem <thiem.huynh0@gmail.com>
2023-03-27 14:47:33 -0400

    morgan install

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import router from './router'
+import morgan from 'morgan'
 
 const express = require('express');
 
 const app = express();
+
+app.use(morgan('dev'))
 
 app.get('/', (req,res) => {
     console.log('hello from express');
